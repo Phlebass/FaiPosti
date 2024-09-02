a94eb1ae2c0843bc4e916d566939e907339edab4
Phlebass <179969958+Phlebass@users.noreply.github.com>
2024-09-02 22:34:12 +0200

    add user input
    per generare la piantina e inserire i nomi

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-let str = "";
-let str_posto = ""
+function genera() {
 
-for(let i=0; i<3; i++) {
-  str += "<div class=\"colonna\">";
-  for (let j=0; j<5; j++) {
-    let posti = 2;
-    if (i==1 && (j==0 || j==1))
-        posti=3;
-    str+= "<div class=\"coppia\">";
-    for(k=0; k<posti; k++) {
-        str_posto = "<div> <input type=\"text\" id=\"b" +i+"b"+j+"b"+k+"\"></input></div>";
-        str += str_posto;
-    }
-    str+="</div>"
+  let str = "";
+  let id_posto = ""
+  let input_cols = document.getElementById("in_format").value.split("; ");
+  let values = [];
+  for (let c = 0; c < input_cols.length; c++) {
+    values[c] = input_cols[c].split(" ");
+    values[c] = values[c].map((x) => parseInt(x));
   }
-  str += "</div>";
+
+  console.log(values);
+
+  for (let i = 0; i < input_cols.length; i++) {
+    str += "<div class=\"colonna\">";
+    for (let j = 0; j < values[i].length; j++) {
+      str += "<div class=\"coppia\">";
+      for (k = 0; k < values[i][j]; k++) {
+        id_posto = "" + i + "-" + j + "-" + k;
+        str += "<div> <input type=\"text\" class=\"testo_banco\" id=\"testo-" + id_posto + "\"></div>";
+        //str += "<input type=\"checkbox\" id=\"cb-" + id_posto + "\"></input></div>";
+      }
+      str += "</div>"
+    }
+    str += "</div>";
+  }
+
+  document.querySelector("#piantina").innerHTML = str;
+
 }
 
-document.querySelector("#content").innerHTML = str;
+function inserisci_nomi() {
+  let nomi = document.querySelector("#in_nomi").value.split(", ");
+  let banchi = document.querySelectorAll(".testo_banco");
+  let skip = 0;
+  let i = 0;
+  while (i < nomi.length) {
+    if (banchi.item(i + skip) == null) {
+      console.log("Non ci sono abbastanza banchi per inserire tutti i nomi");
+      return;
+    } else if (banchi.item(i + skip).value == "") {
+      banchi.item(i + skip).value = nomi[i];
+      i++;
+    } else {
+      skip++;
+    }
+  }
+}
+
+document.querySelector("#in_format").value = "2 2 2 2 2; 2 2 3 3 3; 2 2 2 2";
+document.querySelector("#in_nomi").value = "Sauro Dino, Coteca Pina";
